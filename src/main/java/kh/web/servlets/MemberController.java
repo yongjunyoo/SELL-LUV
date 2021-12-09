f8cbef48c5e9aef873f8b100538aaf662c23cb7e
jhleeeeeeee <gov987@naver.com>
2021-12-09 23:29:07 +0900

    이준협:회원가입선택페이지추가, 멤버컨트롤러 추가

--- src/main/java/kh/web/servlets/MemberController.java
+++ src/main/java/kh/web/servlets/MemberController.java
@@ -20,9 +20,37 @@ public class MemberController extends HttpServlet {
 		System.out.println("사용자가 요청한 기능 : " + cmd);
 		
 		try {
-			if(cmd.equals("")) {
+			if(cmd.equals("/login.mem")) {
+				
+				response.sendRedirect("/resources/login/login.jsp");
+				
+			}else if(cmd.equals("/loginCheck.mem")) {
+				
+				response.sendRedirect("/index.jsp");
+				
+			}else if(cmd.equals("/signup.mem")) {
+				
+				response.sendRedirect("/resources/signup/selectSignup.jsp");
+				
+			}else if(cmd.equals("/CPSignup.mem")) {
+				
+				response.sendRedirect("/resources/signup/CPSignup.jsp");
+				
+			}else if(cmd.equals("/IFSignup.mem")) {
+				
+				response.sendRedirect("/resources/signup/IFSignup.jsp");
+				
+			}else if(cmd.equals("/CPSubmit.mem")) {
+				
+
+				
+			}else if(cmd.equals("/IFSubmit.mem")) {
+				
+
 				
 			}
+			
+			
 		}catch(Exception e) {
 			e.printStackTrace();
 			response.sendRedirect("error.jsp");
